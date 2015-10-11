c8310c57824c29c0b8e4f88b491c52ee94d8f600
alexsimi66 <alexsimi66@gmail.com>
2015-10-11 13:38:51 +0300

    UI improvements

--- licentaAS/WebContent/js/welcome.js
+++ licentaAS/WebContent/js/welcome.js
@@ -1,12 +1,3 @@
 $(window).ready(function(){
-    var WINDOW_WIDTH = window.innerWidth;
-    var slides = $('.slide');
-    slides.css('margin-left', WINDOW_WIDTH);
-    $('.active-slide').css('margin-left', 'auto');
-    
-    $('#next-slide').on('click', function(){
-        $('.active-slide').animate({marginLeft: -WINDOW_WIDTH}, 1000);
-        $('.active-slide+.slide').attr('trfale', 'da');
-//        $('.active-slide+.slide').animate({marginLeft: 'auto'}, 1000);
-    });
+
 });
--- licentaAS/src/ro/asimandi/simsec/controllers/hello.java
+++ licentaAS/src/ro/asimandi/simsec/controllers/hello.java
@@ -2,13 +2,14 @@ package ro.asimandi.simsec.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
 public class hello{
  
 	@RequestMapping("/hello")
-	public String helloWorld() {
+	public String helloWorld(@RequestParam String code) {
+		System.out.println(code);
 		return "hello";
 	}
 }
